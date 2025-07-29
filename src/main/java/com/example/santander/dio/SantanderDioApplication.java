d52c8ef5a87bd5088f954d3cae127423699ffdca
YanFelix <yanfelixpimentel@hotmail.com>
2025-07-29 11:03:20 -0300

    Initial commit

---
+++ src/main/java/com/example/santander/dio/SantanderDioApplication.java
@@ -0,0 +1,13 @@
+package com.example.santander.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/santander/dio/SantanderDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santander.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
